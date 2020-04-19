d6f1a96743d44d3acc3e6328c21535549571aa9b
shanghalf <shanghalf1967@gmail.com>
2020-04-19 10:59:13 +0700

    Add files via upload
    Bridge Dll target   everything stand in App.cs Matrix.cs handling minimalistic Matrix math  ... use nuggets packages to add 
    bridge references , just my 2 cents as translating JS to C# could be difficult .. a good stating point

---
+++ App.cs
@@ -0,0 +1,292 @@
+﻿using Bridge;
+using Bridge.Html5;
+using Bridge.WebGL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace WebGLEditor
+{
+    public class App
+    {
+
+
+
+
+
+
+        public static int VERTEX_SHADER { get; private set; }
+        public static double[] positions = new double[] {
+          // Front face
+          -1.0, -1.0, 1.0,
+           1.0, -1.0, 1.0,
+           1.0, 1.0, 1.0,
+          -1.0, 1.0, 1.0,
+
+
+          // Back face
+          -1.0, -1.0, -1.0,
+          -1.0, 1.0, -1.0,
+           1.0, 1.0, -1.0,
+           1.0, -1.0, -1.0,
+
+          // Top face
+          -1.0, 1.0, -1.0,
+          -1.0, 1.0, 1.0,
+           1.0, 1.0, 1.0,
+           1.0, 1.0, -1.0,
+
+          // Bottom face
+          -1.0, -1.0, -1.0,
+           1.0, -1.0, -1.0,
+           1.0, -1.0, 1.0,
+          -1.0, -1.0, 1.0,
+
+           // Right face
+           1.0, -1.0, -1.0,
+           1.0, 1.0, -1.0,
+           1.0, 1.0, 1.0,
+           1.0, -1.0, 1.0,
+
+          // Left face
+          -1.0, -1.0, -1.0,
+          -1.0, -1.0, 1.0,
+          -1.0, 1.0, 1.0,
+          -1.0, 1.0, -1.0
+        };
+
+
+        const string fsSource =
+            "varying lowp vec4 vColor;"+
+            "void main()" +
+            "{" +
+            "gl_FragColor = vColor;" +
+            "}";
+
+        const string vsSource =
+            "attribute vec4 aVertexPosition;" +
+            "attribute vec4 aVertexColor;" +
+            "uniform mat4 uModelViewMatrix;" +
+            "uniform mat4 uProjectionMatrix;" +
+            "varying lowp vec4 vColor;" +
+            "void main()" +
+            "{" +
+            "gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;" +
+            "vColor = aVertexColor;"+
+            "}";
+
+        public static float then = 0.0f;
+        public static WebGLRenderingContext gl;
+        public static Programinfo P;
+
+        static void TickTimer(object state)
+        {
+
+            then += 0.01f;
+
+            Console.Clear();
+            Console.WriteLine(then.ToString());
+            DrawScene(gl, P, then);
+            
+           // Thread.Sleep(500);
+        }
+
+  
+
+        public static Timer T1 = new Timer(new TimerCallback(TickTimer),null,0,10);
+
+        public static void Main()
+        {
+
+            
+
+
+            // Write a message to the Console
+            Console.WriteLine("Welcome to Bridge.NET");
+
+            HTMLCanvasElement canvas = Document.GetElementById("canvas").As<HTMLCanvasElement>();
+            gl = canvas.GetContext("webgl").As<WebGLRenderingContext>();
+            string message;
+
+            if (gl != null && gl != Script.Undefined)
+            {
+                Console.WriteLine("GL CANVAS INIT");
+            }
+            else
+            {
+                Console.WriteLine("WebGLRenderingContext initialization failed");
+            }
+
+
+                GLStuff(gl);
+        }
+        public static WebGLProgram InitShaderProgram(WebGLRenderingContext gl, string vsource, string fsource)
+        {
+
+            Union<int, WebGLProgram> shaderprogram = gl.CreateProgram();
+
+            WebGLShader vertexshader = LoadShader(gl, gl.VERTEX_SHADER, vsource);
+            WebGLShader fragmentshader = LoadShader(gl, gl.FRAGMENT_SHADER, fsource);
+
+            gl.AttachShader((WebGLProgram)shaderprogram, vertexshader);
+            gl.AttachShader((WebGLProgram)shaderprogram, fragmentshader);
+
+            gl.LinkProgram((WebGLProgram)shaderprogram);
+
+            if ((bool)gl.GetProgramParameter((WebGLProgram)shaderprogram, gl.LINK_STATUS) == false)
+            {
+                Console.WriteLine("cannot link" + gl.GetProgramInfoLog((WebGLProgram)shaderprogram));
+                return null;
+
+            }
+            else
+                Console.WriteLine("linked du cul" + gl.GetProgramInfoLog((WebGLProgram)shaderprogram));
+
+            return (WebGLProgram)shaderprogram;
+
+        }
+
+        public static WebGLProgram Theprog;
+        public static void GLStuff(WebGLRenderingContext gl)
+        {
+            Theprog = InitShaderProgram(gl, vsSource, fsSource);
+            P = new Programinfo(gl, Theprog);
+            gl.ClearColor(0.0, 0.0, 0.0, 1.0);
+            gl.Clear(gl.COLOR_BUFFER_BIT);
+        }
+
+
+        public class Programinfo
+
+
+        {
+            public Programinfo(WebGLRenderingContext glin, WebGLProgram progin)
+            {
+                gl = glin;
+                program = progin;
+
+            }
+            public WebGLProgram program;
+            public WebGLRenderingContext gl;
+            public int vertexPosition { get { return gl.GetAttribLocation(program, "aVertexPosition"); } }
+            public int vertexColor { get { return gl.GetAttribLocation(program, "aVertexColor"); } }
+
+            public WebGLUniformLocation projectionMatrix { get { return gl.GetUniformLocation(program, "uProjectionMatrix"); } }
+            public WebGLUniformLocation modelviewmatrix { get { return gl.GetUniformLocation(program, "uModelViewMatrix"); } }
+
+        }
+
+
+        public static WebGLShader LoadShader(WebGLRenderingContext gl, int type, string source)
+        {
+            WebGLShader shader = gl.CreateShader(type);
+            gl.ShaderSource(shader, source);
+            gl.CompileShader(shader);
+            if ((bool)gl.GetShaderParameter(shader, gl.COMPILE_STATUS) == false)
+            {
+                Console.WriteLine(gl.GetShaderInfoLog(shader));
+                return null;
+            }
+            return shader;
+        }
+
+        public enum BufType
+        {
+            BUFFF_POS = 1,
+            BUFF_COLOR = 2
+        }
+
+        public static WebGLBuffer initBuffers(WebGLRenderingContext gl, BufType bt)
+        {
+            double[] pos = new double[] { -1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0 };
+            double[] colors = new double[] { 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0 };
+
+            WebGLBuffer colorBuffer = gl.CreateBuffer();
+            WebGLBuffer posbuffer = gl.CreateBuffer();
+
+            gl.BindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+            gl.BufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+            gl.BindBuffer(gl.ARRAY_BUFFER, posbuffer);
+            gl.BufferData(gl.ARRAY_BUFFER, new Float32Array(pos), gl.STATIC_DRAW);
+
+            switch (bt)
+            {
+                case BufType.BUFFF_POS:
+                    return posbuffer;
+                case BufType.BUFF_COLOR:
+                    return colorBuffer;
+                default:
+                    return posbuffer;
+                    
+
+            }
+            
+
+
+        }
+
+       
+
+
+
+        public static void DrawScene(WebGLRenderingContext gl, Programinfo P,float deltatime )
+        {
+            gl.ClearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+            gl.ClearDepth(1.0);                 // Clear everything
+            gl.Enable(gl.DEPTH_TEST);           // Enable depth testing
+            gl.DepthFunc(gl.LEQUAL);            // Near things obscure far things
+            // Clear the canvas before we start drawing on it.
+            gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+            double fieldOfView = 45 * Math.PI / 180;   // in radians
+            double aspect = (gl.Canvas.ClientWidth / gl.Canvas.ClientHeight);
+            double zNear = 0.1;
+            double zFar = 100.0;
+            Matrix projectionMatrix = new Matrix();
+            Matrix modelViewMatrix = new Matrix();
+            modelViewMatrix.translate( modelViewMatrix, new double[] { -0, 0, -6 });
+            projectionMatrix.perspective( fieldOfView, aspect, zNear, zFar);
+            modelViewMatrix.Rotate(modelViewMatrix,deltatime,new double[] {0,1,1});
+            //int numcomponent = 2;
+            int Type = gl.FLOAT;
+            bool normalize = false;
+            int stride = 0;
+            int offset = 0;
+
+            gl.BindBuffer(gl.ARRAY_BUFFER, initBuffers(gl,BufType.BUFFF_POS));
+            gl.VertexAttribPointer(P.vertexPosition, 2, Type, normalize, stride, offset);
+            gl.EnableVertexAttribArray(P.vertexPosition);
+
+            gl.BindBuffer(gl.ARRAY_BUFFER, initBuffers(gl, BufType.BUFF_COLOR));
+            gl.VertexAttribPointer(P.vertexColor, 4, Type, normalize, stride, offset);
+            gl.EnableVertexAttribArray(P.vertexColor);
+
+
+
+            gl.UseProgram(Theprog);
+            gl.UniformMatrix4fv(P.projectionMatrix, false, projectionMatrix.Lout);
+            gl.UniformMatrix4fv(P.modelviewmatrix, false, modelViewMatrix.Lout);
+
+            int off = 0;
+            int vertexcount = 4;
+            gl.DrawArrays(gl.TRIANGLE_STRIP, off, vertexcount);
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
---
+++ Matrix.cs
@@ -0,0 +1,208 @@
+﻿using System;
+
+namespace WebGLEditor
+{
+
+    public class Matrix 
+    {
+        public double[] Lout; // matrix line 
+        public double[,] Sout;// matrix square 
+
+        public  double[] Create()
+        {
+            double[] Out = new double[] { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
+            return Out;
+        }
+
+
+
+        public double this[int index]
+        {
+            get { return Lout[index];}
+            set { Lout[index] = index; }
+        }
+
+
+
+
+        public  Matrix()
+        {
+           // identity 
+           Lout = new double[] 
+           { 
+               1, 0, 0, 0, 
+               0, 1, 0, 0, 
+               0, 0, 1, 0, 
+               0, 0, 0, 1 
+           };
+        }
+
+
+        public void  Rotate( Matrix mat, double angle,double[] axis)
+        {
+            double x = axis[0];
+            double y = axis[1];
+            double z = axis[2];
+
+
+            double len = Math.Sqrt(x * x + y * y + z * z);
+            double s = 0;
+            double c = 0;
+            double t = 0;
+            double a00 = 0;
+            double a01 = 0;
+            double a02 = 0;
+            double a03 = 0;
+            double a10 = 0;
+            double a11 = 0;
+            double a12 = 0;
+            double a13 = 0;
+            double a20 = 0;
+            double a21 = 0;
+            double a22 = 0;
+            double a23 = 0;
+            double b00 = 0;
+            double b01 = 0;
+            double b02 = 0;
+            double b10 = 0;
+            double b11 = 0;
+            double b12 = 0;
+            double b20 = 0;
+            double b21 = 0;
+            double b22 = 0;
+
+            if (Math.Abs(len) < 0.000001)
+                return ;
+
+            len = 1 / len;
+            x *= len;
+            y *= len;
+            z *= len;
+
+            s = Math.Sin(angle);
+            c = Math.Cos(angle);
+            t = 1 - c;
+
+            a00 = mat[0]; a01 = mat[1]; a02 = mat[2]; a03 = mat[3];
+            a10 = mat[4]; a11 = mat[5]; a12 = mat[6]; a13 = mat[7];
+            a20 = mat[8]; a21 = mat[9]; a22 = mat[10]; a23 = mat[11];
+
+            // Construct the elements of the rotation matrix
+            b00 = x * x * t + c; b01 = y * x * t + z * s; b02 = z * x * t - y * s;
+            b10 = x * y * t - z * s; b11 = y * y * t + c; b12 = z * y * t + x * s;
+            b20 = x * z * t + y * s; b21 = y * z * t - x * s; b22 = z * z * t + c;
+
+  // Perform rotation-specific matrix multiplication
+            Lout[0] = a00* b00 + a10* b01 + a20* b02;
+            Lout[1] = a01* b00 + a11* b01 + a21* b02;
+            Lout[2] = a02* b00 + a12* b01 + a22* b02;
+            Lout[3] = a03* b00 + a13* b01 + a23* b02;
+            Lout[4] = a00* b10 + a10* b11 + a20* b12;
+            Lout[5] = a01* b10 + a11* b11 + a21* b12;
+            Lout[6] = a02* b10 + a12* b11 + a22* b12;
+            Lout[7] = a03* b10 + a13* b11 + a23* b12;
+            Lout[8] = a00* b20 + a10* b21 + a20* b22;
+            Lout[9] = a01* b20 + a11* b21 + a21* b22;
+            Lout[10] = a02* b20 + a12* b21 + a22* b22;
+            Lout[11] = a03* b20 + a13* b21 + a23* b22;
+
+            if (mat.Lout != Lout) {
+                // If the source and destination differ, copy the unchanged last row
+                Lout[12] = mat[12];
+                Lout[13] = mat[13];
+                Lout[14] = mat[14];
+                Lout[15] = mat[15];
+            }
+}
+
+
+public void Rotate ( Matrix a , double angle )
+        {
+            double s = Math.Sin(angle);
+            double  c = Math.Cos(angle);
+
+            Lout[0] = c* a[0] + s* a[3];
+            Lout[1] = c* a[1] + s* a[4];
+            Lout[2] = c* a[2] + s* a[5];
+
+            Lout[3] = c* a[3] - s* a[0];
+            Lout[4] = c* a[4] - s* a[1];
+            Lout[5] = c* a[5] - s* a[2];
+
+            Lout[6] = a[6];
+            Lout[7] = a[7];
+            Lout[8] = a[8];
+
+
+        }
+
+
+
+        public void perspective( double fovy, double aspect, double near, double far)
+        {
+            double f = 1.0 / Math.Tan(fovy / 2);
+            double nf = 1 / (near - far);
+            Lout[0] = f / aspect;
+            Lout[1] = 0;
+            Lout[2] = 0;
+            Lout[3] = 0;
+            Lout[4] = 0;
+            Lout[5] = f;
+            Lout[6] = 0;
+            Lout[7] = 0;
+            Lout[8] = 0;
+            Lout[9] = 0;
+            Lout[10] = (far + near) * nf;
+            Lout[11] = -1;
+            Lout[12] = 0;
+            Lout[13] = 0;
+            Lout[14] = 2 * far * near * nf;
+            Lout[15] = 0;
+
+        }
+
+        public void translate( Matrix a, double[] v)
+        {
+            double x = v[0], y = v[1], z = v[2];
+            double a00 = 0;
+            double a01 = 0;
+            double a02 = 0;
+            double a03 = 0;
+            double a10 = 0;
+            double a11 = 0;
+            double a12 = 0;
+            double a13 = 0;
+            double a20 = 0;
+            double a21 = 0;
+            double a22 = 0;
+            double a23 = 0;
+
+
+            if (a.Lout == Lout)
+            {
+                Lout[12] = a[0] * x + a[4] * y + a[8] * z + a[12];
+                Lout[13] = a[1] * x + a[5] * y + a[9] * z + a[13];
+                Lout[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
+                Lout[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
+            }
+            else
+            {
+                a00 = a[0]; a01 = a[1]; a02 = a[2]; a03 = a[3];
+                a10 = a[4]; a11 = a[5]; a12 = a[6]; a13 = a[7];
+                a20 = a[8]; a21 = a[9]; a22 = a[10]; a23 = a[11];
+
+                Lout[0] = a00; Lout[1] = a01; Lout[2] = a02; Lout[3] = a03;
+                Lout[4] = a10; Lout[5] = a11; Lout[6] = a12; Lout[7] = a13;
+                Lout[8] = a20; Lout[9] = a21; Lout[10] = a22; Lout[11] = a23;
+
+                Lout[12] = a00 * x + a10 * y + a20 * z + a[12];
+                Lout[13] = a01 * x + a11 * y + a21 * z + a[13];
+                Lout[14] = a02 * x + a12 * y + a22 * z + a[14];
+                Lout[15] = a03 * x + a13 * y + a23 * z + a[15];
+            }
+
+        }
+
+
+    }
+}
